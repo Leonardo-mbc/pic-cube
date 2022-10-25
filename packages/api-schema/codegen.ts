3dddf08d42fc01071676eec689d1e1264811065d
inoutch <akiandaki@gmail.com>
2022-10-25 23:18:40 +0900

    Add api schema package

---
+++ packages/api-schema/codegen.ts
@@ -0,0 +1,18 @@
+import type { CodegenConfig } from '@graphql-codegen/cli';
+
+const config: CodegenConfig = {
+  overwrite: true,
+  schema: './graphql/types/**/*.graphql',
+  generates: {
+    './graphql/generated/server.type.ts': {
+      plugins: ['typescript', 'typescript-resolvers', 'typescript-operations'],
+    },
+    // HACK: Direct output because TS of external packages cannot be transpile
+    '../frontend/graphql/generated/client.type.ts': {
+      documents: './graphql/**/*.graphql',
+      plugins: ['typescript', 'typescript-operations', 'typescript-graphql-request'],
+    },
+  },
+};
+
+export default config;
