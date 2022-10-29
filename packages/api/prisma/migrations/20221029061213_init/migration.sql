ac3832d797e1d4faae3f2792ec8d5975c477f281
inoutch <akiandaki@gmail.com>
2022-10-29 16:10:19 +0900

    Update initial schema

--- packages/api/prisma/migrations/20221029061213_init/migration.sql
+++ packages/api/prisma/migrations/20221029061213_init/migration.sql
@@ -1,8 +1,9 @@
 -- CreateTable
 CREATE TABLE `contents` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
     `type` ENUM('COLLECTION', 'ALBUM', 'FILE') NOT NULL,
-    `removed` BOOLEAN NOT NULL,
+    `removed` BOOLEAN NOT NULL DEFAULT false,
     `createdAt` DATETIME(3) NOT NULL,
     `updatedAt` DATETIME(3) NOT NULL,
 
@@ -13,6 +14,10 @@ CREATE TABLE `contents` (
 CREATE TABLE `collections` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `contentId` INTEGER NOT NULL,
+    `lastAccessedAt` DATETIME(3) NOT NULL,
+    `lastModifiedAt` DATETIME(3) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL,
+    `updatedAt` DATETIME(3) NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -34,7 +39,6 @@ CREATE TABLE `collectionContents` (
 CREATE TABLE `albums` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `contentId` INTEGER NOT NULL,
-    `name` VARCHAR(191) NOT NULL,
     `lastAccessedAt` DATETIME(3) NOT NULL,
     `lastModifiedAt` DATETIME(3) NOT NULL,
     `createdAt` DATETIME(3) NOT NULL,
@@ -47,6 +51,7 @@ CREATE TABLE `albums` (
 CREATE TABLE `albumContents` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `contentId` INTEGER NOT NULL,
+    `albumId` INTEGER NOT NULL,
     `createdAt` DATETIME(3) NOT NULL,
     `updatedAt` DATETIME(3) NOT NULL,
 
@@ -57,6 +62,10 @@ CREATE TABLE `albumContents` (
 CREATE TABLE `files` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `contentId` INTEGER NOT NULL,
+    `path` VARCHAR(191) NOT NULL,
+    `filename` VARCHAR(191) NOT NULL,
+    `lastAccessedAt` DATETIME(3) NOT NULL,
+    `lastModifiedAt` DATETIME(3) NOT NULL,
     `createdAt` DATETIME(3) NOT NULL,
     `updatedAt` DATETIME(3) NOT NULL,
 
@@ -70,10 +79,16 @@ ALTER TABLE `collections` ADD CONSTRAINT `collections_contentId_fkey` FOREIGN KE
 ALTER TABLE `collectionContents` ADD CONSTRAINT `collectionContents_contentId_fkey` FOREIGN KEY (`contentId`) REFERENCES `contents`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
+ALTER TABLE `collectionContents` ADD CONSTRAINT `collectionContents_collectionId_fkey` FOREIGN KEY (`collectionId`) REFERENCES `collections`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
 ALTER TABLE `albums` ADD CONSTRAINT `albums_contentId_fkey` FOREIGN KEY (`contentId`) REFERENCES `contents`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `albumContents` ADD CONSTRAINT `albumContents_contentId_fkey` FOREIGN KEY (`contentId`) REFERENCES `contents`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `albumContents` ADD CONSTRAINT `albumContents_albumId_fkey` FOREIGN KEY (`albumId`) REFERENCES `albums`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `files` ADD CONSTRAINT `files_contentId_fkey` FOREIGN KEY (`contentId`) REFERENCES `contents`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
