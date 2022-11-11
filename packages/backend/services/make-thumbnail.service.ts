109a47c952e913b57414fd7def0a224bd4c0d237
LeonardoKen Orihara <leonardo.mbc17@gmail.com>
2022-11-12 02:22:54 +0900

    Create a reusable function runFFmpeg

--- packages/backend/services/make-thumbnail.service.ts
+++ packages/backend/services/make-thumbnail.service.ts
@@ -90,89 +90,90 @@ export async function makeThumbnail(
       }
     }
 
-    ffmpeg(path)
-      .seekInput(seekTo)
-      .complexFilter([
-        {
-          filter: 'scale',
-          options: `'if(lt(a,1),-1,${rectSize})':'if(lt(a,1),${rectSize},-1)'`,
-          inputs: ['0:v'],
-          outputs: ['contain'],
-        },
-        {
-          filter: 'scale',
-          options: `'if(lt(a,1/1),${rectSize},-1)':'if(lt(a,1/1),-1,${rectSize})'`,
-          inputs: ['0:v'],
-          outputs: ['cover'],
-        },
-        {
-          filter: 'crop',
-          options: [rectSize, rectSize],
-          inputs: ['cover'],
-          outputs: ['cover'],
-        },
-      ])
-      .output(containPath)
-      .map('contain')
-      .videoCodec('mjpeg')
-      .outputOptions('-vframes 1')
-      .outputOptions('-pix_fmt yuv420p')
-      .outputOptions('-f image2pipe')
+    function runFFmpeg(seekTo: number) {
+      ffmpeg(path)
+        .seekInput(seekTo)
+        .complexFilter([
+          {
+            filter: 'scale',
+            options: `'if(lt(a,1),-1,${rectSize})':'if(lt(a,1),${rectSize},-1)'`,
+            inputs: ['0:v'],
+            outputs: ['contain'],
+          },
+          {
+            filter: 'scale',
+            options: `'if(lt(a,1/1),${rectSize},-1)':'if(lt(a,1/1),-1,${rectSize})'`,
+            inputs: ['0:v'],
+            outputs: ['cover'],
+          },
+          {
+            filter: 'crop',
+            options: [rectSize, rectSize],
+            inputs: ['cover'],
+            outputs: ['cover'],
+          },
+        ])
+        .output(containPath)
+        .map('contain')
+        .videoCodec('mjpeg')
+        .outputOptions('-vframes 1')
+        .outputOptions('-pix_fmt yuv420p')
+        .outputOptions('-f image2pipe')
 
-      .output(coverPath)
-      .map('cover')
-      .videoCodec('mjpeg')
-      .outputOptions('-vframes 1')
-      .outputOptions('-pix_fmt yuv420p')
-      .outputOptions('-f image2pipe')
+        .output(coverPath)
+        .map('cover')
+        .videoCodec('mjpeg')
+        .outputOptions('-vframes 1')
+        .outputOptions('-pix_fmt yuv420p')
+        .outputOptions('-f image2pipe')
 
-      .on('error', (errorMessage) => {
-        console.error('[MAKE_THUMB:FFMPEG_ERROR]', path, errorMessage);
-        return reject({
-          status: MT_STATUS_ERROR,
-          errorMessage,
-        });
-      })
-      .on('end', async () => {
-        if (outputMeta) {
-          const colors = await getColors(containPath, {
-            count: 10,
-            type: 'image/jpg',
+        .on('error', (errorMessage) => {
+          console.error('[MAKE_THUMB:FFMPEG_ERROR]', path, errorMessage);
+          return reject({
+            status: MT_STATUS_ERROR,
+            errorMessage,
           });
-          const darknessAverage = colors.reduce((prev, color) => prev + color.hsl()[2] / 10, 0);
-
-          if ((darknessAverage < 0.4 || darknessAverage > 0.8) && seekTo < 5) {
-            console.log('[MAKE_THUMB:REMAKE]', metaDir, seekTo, darknessAverage);
-            return makeThumbnail(path, {
-              ...options,
-              seekTo: seekTo + 1,
+        })
+        .on('end', async () => {
+          if (outputMeta) {
+            const colors = await getColors(containPath, {
+              count: 10,
+              type: 'image/jpg',
             });
-          } else {
-            await fs.promises.writeFile(hashPath, await makeFileHash(path));
+            const darknessAverage = colors.reduce((prev, color) => prev + color.hsl()[2] / 10, 0);
 
-            console.log('[MAKE_THUMB:OUTPUT_META]', metaDir, seekTo, darknessAverage);
-            return resolve({
-              status: MT_STATUS_OUTPUT_META,
-              containPath,
-              coverPath,
-            });
+            if ((darknessAverage < 0.4 || darknessAverage > 0.8) && seekTo < 5) {
+              console.log('[MAKE_THUMB:REMAKE]', metaDir, seekTo, darknessAverage);
+              return runFFmpeg(seekTo + 1);
+            } else {
+              await fs.promises.writeFile(hashPath, await makeFileHash(path));
+
+              console.log('[MAKE_THUMB:OUTPUT_META]', metaDir, seekTo, darknessAverage);
+              return resolve({
+                status: MT_STATUS_OUTPUT_META,
+                containPath,
+                coverPath,
+              });
+            }
           }
-        }
 
-        const [containBuffer, coverBuffer] = await Promise.all([
-          fs.promises.readFile(containPath),
-          fs.promises.readFile(coverPath),
-        ]);
+          const [containBuffer, coverBuffer] = await Promise.all([
+            fs.promises.readFile(containPath),
+            fs.promises.readFile(coverPath),
+          ]);
 
-        fs.rmSync(metaDir, { recursive: true, force: true });
+          fs.rmSync(metaDir, { recursive: true, force: true });
 
-        console.log('[MAKE_THUMB:RETURN_BUFFER]', metaDir);
-        return resolve({
-          status: MT_STATUS_RETURN_BUFFER,
-          containBuffer,
-          coverBuffer,
-        });
-      })
-      .run();
+          console.log('[MAKE_THUMB:RETURN_BUFFER]', metaDir);
+          return resolve({
+            status: MT_STATUS_RETURN_BUFFER,
+            containBuffer,
+            coverBuffer,
+          });
+        })
+        .run();
+    }
+
+    runFFmpeg(seekTo);
   });
 }
